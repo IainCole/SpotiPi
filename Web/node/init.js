811e7ea2f79182a77734c1f07b70bd40a4ada3bc
Iain Cole <iaincole1@gmail.com>
2013-01-16 21:22:27 +0000

    Included css / js, and now running on the pi!

--- Web/node/init.js
+++ Web/node/init.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var fs = require('fs');
 var io = require('socket.io');
+var url = require('url');
 
 var config, lang;
 
@@ -30,12 +31,31 @@ console.log('Language Pack Loaded');
 
 // Set Up HTTP Server
 var app = http.createServer(function (req, res) {
-	fs.readFile('./html/index.html', function (err, data) {
+	var fileName = './html/index.html';
+	var contentType = 'text/html';
+
+	// We could build something that goes and serves files that are requested but this is basically everything, so what's the point
+	switch (url.parse(req.url).pathname) {
+		case '/SpotiPi.js':
+			fileName = './html/js/SpotiPi.js';
+			contentType = 'application/javascript';
+		break;
+		case '/jquery.js':
+			fileName = './html/js/jquery.js';
+			contentType = 'application/javascript';
+		break;
+		case '/SpotiPi.css':
+			fileName = './html/css/SpotiPi.css';
+			contentType = 'text/css';
+		break;
+	}
+
+	fs.readFile(fileName, function (err, data) {
 		if (err) {
-			res.writeHead(404, {'Content-Type': 'text/plain'});
+			res.writeHead(404, { 'Content-Type': 'text/plain' });
 			res.end(lang.FileNotFound);
 		} else {
-			res.writeHead(200, {'Content-Type': 'text/html'});
+			res.writeHead(200, { 'Content-Type': contentType });
 			res.end(data);
 		}
 	});
